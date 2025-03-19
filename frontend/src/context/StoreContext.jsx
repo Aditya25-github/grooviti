a05bec98381e74094678395128a382241c1dc258
Aditya25-github <144470959+Aditya25-github@users.noreply.github.com>
2025-03-20 02:51:22 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://grooviti-backend.onrender.com";
   const [token, setToken] = useState("");
   const [myevents_list, setmyevents_list] = useState([]);
 
