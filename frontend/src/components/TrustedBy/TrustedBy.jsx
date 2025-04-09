b32964e067da857c6df1b9f1d8db196a577140cf
siddhipankhade <pankhade.siddhi@gmail.com>
2025-04-09 16:39:35 +0530

    chnge

--- frontend/src/components/TrustedBy/TrustedBy.jsx
+++ frontend/src/components/TrustedBy/TrustedBy.jsx
@@ -6,16 +6,29 @@ const logos = [
   { image: "/images/pcet_logo.png", alt: "PCET Trust Logo" }
 ];
 
+
 const TrustedBy = () => {
+  
+const repeatedLogos = [...logos, { spacer: true }, ...logos];
+
   return (
     <div className="trusted-container">
       <h2 className="textt">Trusted by</h2>
       <div className="marquee-wrapper">
         <div className="marquee">
           <div className="marquee-content">
-            {logos.map((logo, index) => (
-              <img key={index} src={logo.image} alt={logo.alt} className="trust-logo" />
-            ))}
+            {repeatedLogos.map((logo, index) =>
+              logo.spacer ? (
+                <div key={index} style={{ width: "65vw" }}></div>
+              ) : (
+                <img
+                  key={index}
+                  src={logo.image}
+                  alt={logo.alt}
+                  className="trust-logo"
+                />
+              )
+            )}
           </div>
         </div>
       </div>
