c349eed53e0910abb330219e28be38e3add19a8c
Aditya25-github <144470959+Aditya25-github@users.noreply.github.com>
2025-06-24 16:27:07 +0530

    few more changes

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -62,7 +62,7 @@ const List = ({ url }) => {
         </div>
         {list.map((item) => (
           <div key={item._id} className="list-table-format">
-            <img src={`${url}/images/${item.image}`} alt={item.name} />
+            <img src={`${url}/uploads/${item.coverImage}`} alt={item.name} />
             <p>{item.name}</p>
             <p>{item.category}</p>
             <p>Rs.{item.price}</p>
