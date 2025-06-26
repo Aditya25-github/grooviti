14bc0c44822d33f8f326fb277326db35729b0b73
Aditya25-github <144470959+Aditya25-github@users.noreply.github.com>
2025-06-26 17:11:34 +0530

    forgot to change backend links

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://grooviti-backend.onrender.com";
   const [token, setToken] = useState("");
   const [user, setUser] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
