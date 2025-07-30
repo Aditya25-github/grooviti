25ca79858881931f450cd6273284dc3fe37a6cd5
Aditya25-github <144470959+Aditya25-github@users.noreply.github.com>
2025-07-30 12:13:17 +0530

    Venues updated in sports

--- sports/src/App.jsx
+++ sports/src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Navbar from './components/NavBar/NavBar.jsx';
-import Footer from './components/Footer/Footer.jsx';
-import Aurora from './components/Aurora/Aurora';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/NavBar/NavBar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Aurora from "./components/Aurora/Aurora";
 // Pages
-import Home from './pages/Home/Home';
-import AboutUs from './pages/AboutUs/AboutUs';
-import Venues from './pages/Venues/venues';
-
+import Home from "./pages/Home/Home";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Venues from "./pages/Venues/Venues.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
   const location = useLocation();
-  const isHomePage = location.pathname === '/';
+  const isHomePage = location.pathname === "/";
 
   return (
     <div className="App">
@@ -23,7 +22,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutUs" element={<AboutUs />} />
-        <Route path="/Venues" element={<Venues/>}/>
+        <Route path="/Venues" element={<Venues />} />
       </Routes>
       <Footer />
     </div>
