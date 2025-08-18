c7183addb29b4cd5e6b1a2852b85ca4f5951c445
Swaroop Mane <swaroopmane21@gmail.com>
2025-08-18 09:13:30 +0530

    ahh changing link

--- sports/src/context/StoreContext.jsx
+++ sports/src/context/StoreContext.jsx
@@ -6,8 +6,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  // const url = "https://grooviti-backend.onrender.com";
-  const url = "http://localhost:4000"; // Change this to your backend URL
+  const url = "https://grooviti-backend.onrender.com";
+  // const url = "http://localhost:4000"; // Change this to your backend URL
   const [token, setToken] = useState(() => localStorage.getItem("token") || "");
   const [user, setUser] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
