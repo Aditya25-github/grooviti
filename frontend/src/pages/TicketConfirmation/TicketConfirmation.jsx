7b880693e7a2bf9b3500358101b0af29a7c733dd
Swaroop Mane <swaroopmane21@gmail.com>
2026-03-31 01:00:51 +0530

    ticket confirmation page error solved

--- frontend/src/pages/TicketConfirmation/TicketConfirmation.jsx
+++ frontend/src/pages/TicketConfirmation/TicketConfirmation.jsx
@@ -20,10 +20,15 @@ const TicketConfirmation = () => {
   useEffect(() => {
     const fetchOrderDetails = async () => {
       try {
-        const response = await axios.post(`${url}/api/booking/order-details`, {
-          orderId,
-        });
-
+        const response = await axios.post(
+          `${url}/api/booking/order-details`,
+          { orderId },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          },
+        );
         if (response.data.success) {
           setOrderDetails(response.data.order);
         } else {
@@ -42,62 +47,63 @@ const TicketConfirmation = () => {
   }, [orderId, url]);
 
   return (
-    <div style={{paddingTop:"95px"}}>
-    <div className="ticket-confirmation">
-      {loading ? (
-        <div className="spinner"></div>
-      ) : error ? (
-        <div className="error-message">{error}</div>
-      ) : (
-        <div className="ticket-details">
-          <h2>🎟️ Ticket Confirmation</h2>
-          <p>
-            <b>
-              Hey {orderDetails?.address.firstName} ! Your ticket has been
-              booked successfully! Thank you for purchasing tickets on
-              grooviti.com.
-            </b>
-          </p>
+    <div style={{ paddingTop: "95px" }}>
+      <div className="ticket-confirmation">
+        {loading ? (
+          <div className="spinner"></div>
+        ) : error ? (
+          <div className="error-message">{error}</div>
+        ) : (
+          <div className="ticket-details">
+            <h2>🎟️ Ticket Confirmation</h2>
+            <p>
+              <b>
+                Hey {orderDetails?.address.firstName} ! Your ticket has been
+                booked successfully! Thank you for purchasing tickets on
+                grooviti.com.
+              </b>
+            </p>
 
-          <div className="seperator"></div>
+            <div className="seperator"></div>
 
-          <h3>Booking Details</h3>
-          <p>
-            <strong className="order">Order ID:</strong> {orderDetails?.orderId}
-          </p>
-          <p>
-            <strong className="name">Name:</strong>{" "}
-            {orderDetails?.address.firstName} {orderDetails?.address.lastName}
-          </p>
-          <p>
-            <strong className="email">Email:</strong>{" "}
-            {orderDetails?.address.email}
-          </p>
-          <p>
-            <strong className="phone">Phone:</strong>{" "}
-            {orderDetails?.address.phone}
-          </p>
+            <h3>Booking Details</h3>
+            <p>
+              <strong className="order">Order ID:</strong>{" "}
+              {orderDetails?.orderId}
+            </p>
+            <p>
+              <strong className="name">Name:</strong>{" "}
+              {orderDetails?.address.firstName} {orderDetails?.address.lastName}
+            </p>
+            <p>
+              <strong className="email">Email:</strong>{" "}
+              {orderDetails?.address.email}
+            </p>
+            <p>
+              <strong className="phone">Phone:</strong>{" "}
+              {orderDetails?.address.phone}
+            </p>
 
-          <div className="seperator"></div>
+            <div className="seperator"></div>
 
-          <h3>Event Details</h3>
-          <ul>
-            {orderDetails?.items.map((item, index) => (
-              <li key={index}>
-                {item.name} - <b>Qty:</b> {item.quantity}
-              </li>
-            ))}
-          </ul>
-          <p>
-            <strong className="total">Total Amount:</strong> ₹
-            {orderDetails?.amount}
-          </p>
-          <p>
-            <strong className="status">Payment Status:</strong> ✅ Confirmed
-          </p>
-        </div>
-      )}
-    </div>
+            <h3>Event Details</h3>
+            <ul>
+              {orderDetails?.items.map((item, index) => (
+                <li key={index}>
+                  {item.name} - <b>Qty:</b> {item.quantity}
+                </li>
+              ))}
+            </ul>
+            <p>
+              <strong className="total">Total Amount:</strong> ₹
+              {orderDetails?.amount}
+            </p>
+            <p>
+              <strong className="status">Payment Status:</strong> ✅ Confirmed
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
